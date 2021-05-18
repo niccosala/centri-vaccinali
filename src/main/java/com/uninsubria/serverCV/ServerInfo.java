6aba6044b874884c7d158fe191a5c421c6a7e5b2
magau <magauddag@protonmail.com>
2021-05-18 12:11:39 +0200

    [2.3.5.18-SNAPSHOT] minor updates and fixes

---
+++ src/main/java/com/uninsubria/serverCV/ServerInfo.java
@@ -0,0 +1,15 @@
+/*
+Franchi   Matteo    740760   VA
+Magaudda  Giovanni  740962   VA
+Sala      Niccolò   742545   VA
+ */
+
+package com.uninsubria.serverCV;
+
+public class ServerInfo {
+    static final int PORT = 7070;
+    static final String PG_USERNAME = "postgres";
+    static final String PG_PASSWORD = "pass";
+    static final String DB_NAME = "cv";
+    static final String IP_SERVER = "127.0.0.1";
+}
